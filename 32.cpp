f1f8d37453624255a79c8e9b1dba4305700afdea
ilyas8778 <mister.tolegenov@gmail.com>
2019-11-19 12:48:49 +0600

    Some edits

---
+++ 32.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main (){
+int a = 237;
+cout<<37/10;
+int x = (a%100)*10+a/100;
+cout<<x;
+}
---
+++ 33.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (){
+int x,y;
+cin>>x>>y;
+double e = sqrt(x-sqrt(y));
+cout<<e;
+}
